6ca62b904e67d15ed192235d687976f0046a609e
Andrey Stepanov <standmit@yandex.ru>
2024-12-16 17:31:47 +0500

    add chatcommand

--- init.lua
+++ init.lua
@@ -1,18 +1,33 @@
+local function get_data(name, pos)
+	local biomedata = core.get_biome_data(pos)
 
-core.register_craftitem("get_biome_data:wand", {
-	description = "Biome Data Wand\n(right-click to get biome data in node's position)",
-	inventory_image = "get_biome_data_wand.png",
-	groups = {tool = 1},
-	on_place = function(itemstack, player, pointed_thing)
-		local biomedata = core.get_biome_data(pointed_thing.under)
-
-		local str =
-				core.colorize("#aaa", "Biome: ")
+	core.chat_send_player(
+		name,
+		core.colorize("#aaa", "Biome: ")
 			 .. core.colorize("#0f0", core.get_biome_name(biomedata.biome))
 			 .. " ( heat: " .. core.colorize("#f00", biomedata.heat) .. ", humidity: " .. core.colorize("#55f", biomedata.humidity) .. ")"
+	)
+end
 
-		core.chat_send_player(player:get_player_name(), str)
+core.register_tool("get_biome_data:wand", {
+	description = "Biome Data Wand\n(right-click to get biome data in node's position)",
+	inventory_image = "get_biome_data_wand.png",
+	on_place = function(itemstack, player, pointed_thing)
+		get_data(player:get_player_name(), pointed_thing.under)
 
 		return itemstack
 	end,
+})
+
+core.register_chatcommand("get_biome_data", {
+	description = "Get biome data in current position",
+	privs = {
+		debug = true,
+	},
+	func = function(name, params)
+		get_data(
+			name,
+			core.get_player_by_name(name):get_pos()
+		)
+	end
 })

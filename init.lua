769ab5ec744f42bea674783b35f118b5229fb7f6
ROllerozxa <rollerozxa@voxelmanip.se>
2024-12-21 18:39:24 +0100

    Small formatting fixes

--- init.lua
+++ init.lua
@@ -5,8 +5,7 @@ local function get_data(name, pos)
 		name,
 		core.colorize("#aaa", "Biome: ")
 			 .. core.colorize("#0f0", core.get_biome_name(biomedata.biome))
-			 .. " ( heat: " .. core.colorize("#f00", biomedata.heat) .. ", humidity: " .. core.colorize("#55f", biomedata.humidity) .. ")"
-	)
+			 .. " ( heat: " .. core.colorize("#f00", biomedata.heat) .. ", humidity: " .. core.colorize("#55f", biomedata.humidity) .. ")")
 end
 
 core.register_tool("get_biome_data:wand", {
@@ -21,13 +20,8 @@ core.register_tool("get_biome_data:wand", {
 
 core.register_chatcommand("get_biome_data", {
 	description = "Get biome data in current position",
-	privs = {
-		debug = true,
-	},
+	privs = {debug = true},
 	func = function(name, params)
-		get_data(
-			name,
-			core.get_player_by_name(name):get_pos()
-		)
+		get_data(name, core.get_player_by_name(name):get_pos())
 	end
 })
